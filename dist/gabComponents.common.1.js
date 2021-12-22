25cdfc41d87c60132a4226541b5748b9a7667ca9
Gabriel Dantas <gabrielmd123@gmail.com>
2021-12-22 15:02:37 -0300

    testing importing scss with webpack

---
+++ dist/gabComponents.common.1.js
@@ -0,0 +1,11 @@
+((typeof self !== 'undefined' ? self : this)["webpackJsonpgabComponents"] = (typeof self !== 'undefined' ? self : this)["webpackJsonpgabComponents"] || []).push([[1],{
+
+/***/ "41d0":
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+
+}]);
+//# sourceMappingURL=gabComponents.common.1.js.map
--- src/use.js
+++ src/use.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 
 import './plugins/bootstrap-vue'
-import './app.scss'
 
 Vue.config.productionTip = false
 
 import App from './App.vue'
 
 export default (selector) => {
-    new Vue({
-        render: h => h(App),
-    }).$mount(selector)
+    import('./app.scss').then(() => {
+        new Vue({
+            render: h => h(App),
+        }).$mount(selector)
+
+    })
 }
