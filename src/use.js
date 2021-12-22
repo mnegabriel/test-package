4b85af4874ce2db23197d8042724de888ae61101
Gabriel Dantas <gabrielmd123@gmail.com>
2021-12-22 15:12:07 -0300

    going back to test accessing the scss in the package

--- src/use.js
+++ src/use.js
@@ -1,16 +1,17 @@
 import Vue from 'vue'
 
 import './plugins/bootstrap-vue'
+import './app.scss'
 
 Vue.config.productionTip = false
 
 import App from './App.vue'
 
 export default (selector) => {
-    import('./app.scss').then(() => {
+    // import('./app.scss').then(() => {
         new Vue({
             render: h => h(App),
         }).$mount(selector)
 
-    })
+    // })
 }
